b48d4f3fb633ac41b4dbea8b09e563503e19e8b7
Yifeng Mao <mao.yife15@gmail.com>
2020-08-13 00:09:47 -0400

    glitch fixed

--- src/pages/PlasticsEntry.jsx
+++ src/pages/PlasticsEntry.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 
-
 const PlasticsEntry = () => {
     return (
         <main>
   			<div id = "image">
   			    <p> abc </p>
-    			<img style='width: 100vw;' id="background" src="https://d3n8a8pro7vhmx.cloudfront.net/boomerangalliance/pages/3751/attachments/original/1548737070/How_does_plastic_end_up_in_the_ocean__Poster_3.0_Free_digital-1.jpg?1548737070">
-  			    </img>
+    			<img id="background" src="https://d3n8a8pro7vhmx.cloudfront.net/boomerangalliance/pages/3751/attachments/original/1548737070/How_does_plastic_end_up_in_the_ocean__Poster_3.0_Free_digital-1.jpg?1548737070"/>
   			</div>
 		</main>
     )
